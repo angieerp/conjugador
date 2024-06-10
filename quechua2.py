26ded6cc9440e3253c6c76c6f3569d33166a2d3d
angieerp <arodriguezp@pucp.edu.pe>
2024-06-10 17:21:04 -0500

    Update quechua2.py

--- quechua2.py
+++ quechua2.py
@@ -46,7 +46,24 @@ dfp.set_index(c[0], inplace=True)
 dp = dfp.to_dict()
 
 def conjugacion2(base,numero,persona,tiempo):
-  return dp[numero][persona] + ' ' + base + D[tiempo][numero][persona]
+    
+    if numero not in dp:
+         st.error(f"Clave '{numero}' no encontrada en el diccionario 'dp'.")
+         return
+    if persona not in dp[numero]:
+         st.error(f"Clave '{persona}' no encontrada en el diccionario anidado dentro de 'dp[{numero}]'.")
+         return
+    if tiempo not in D:
+         st.error(f"Clave '{tiempo}' no encontrada en el diccionario 'D'.")
+         return
+    if numero not in D[tiempo]:
+         st.error(f"Clave '{numero}' no encontrada en el diccionario anidado dentro de 'D[{tiempo}]'.")
+         return
+    if persona not in D[tiempo][numero]:
+         st.error(f"Clave '{persona}' no encontrada en el diccionario anidado dentro de 'D[{tiempo}][{numero}]'.")
+         return
+    return dp[numero][persona] + ' ' + base + D[tiempo][numero][persona]
+    
 
 #######################################################################
 #######################################################################
